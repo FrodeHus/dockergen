6b9e3080a3458e9b2cd04ec4a2a4d6b34f137518
Frode Hus <frode.hus@outlook.com>
2021-06-11 21:00:14 +0200

    added mudblazor

--- src/DockerGen/Program.cs
+++ src/DockerGen/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace DockerGen
 {
@@ -18,7 +19,7 @@ namespace DockerGen
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
