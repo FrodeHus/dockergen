61079f77d019227ca629aefaf2dd244934ef8b68
Frode Hus <frode.hus@outlook.com>
2021-06-20 01:16:56 +0200

    started work on ui

---
+++ src/DockerGen/Components/Image.razor.cs
@@ -0,0 +1,10 @@
+using DockerGen.Container;
+using Microsoft.AspNetCore.Components;
+
+namespace DockerGen.Components
+{
+    public partial class Image : ComponentBase
+    {
+        public ContainerImage ContainerImage { get; set; }
+    }
+}
