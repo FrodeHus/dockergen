f5eda0e0f8cde94ff18a5e29a39507b11b3ba08a
frodehus <frode.hus@outlook.com>
2021-08-18 10:00:55 +0200

    added support for authentication added boilerplate web api

--- src/DockerGen/Program.cs
+++ src/DockerGen/Program.cs
@@ -3,6 +3,7 @@ using DockerGen.Container;
 using DockerGen.Container.Recipes;
 using DockerGen.Infrastructure;
 using Fluxor;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace DockerGen
@@ -13,6 +14,9 @@ namespace DockerGen
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.Services.AddHttpClient("DockerGen.Api", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DockerGen.Api"));
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<RecipeLoader>();
@@ -22,6 +26,14 @@ namespace DockerGen
             {
                 config.JsonSerializerOptions.WriteIndented = true;
             });
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+            });
+
             builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly).UseReduxDevTools());
             await builder.Build().RunAsync();
         }
