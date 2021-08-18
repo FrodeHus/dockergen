c2763336396805c0fddb1ee12f432a9d8d3392c1
frodehus <frode.hus@outlook.com>
2021-08-18 21:49:37 +0200

    create redis cluster script

---
+++ createrediscluster.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+kubectl exec -it redis-cluster-0 -- redis-cli --cluster create --cluster-replicas 1 $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
