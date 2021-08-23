bb359195c73770c52ca19503f74e2ff7db08f920
frodehus <frode.hus@outlook.com>
2021-08-23 21:35:54 +0200

    removed default base image to force a choice between existing build stage and new image

--- src/DockerGen.Shared/Container/Instructions/AddFilesInstruction.cs
+++ src/DockerGen.Shared/Container/Instructions/AddFilesInstruction.cs
@@ -5,10 +5,11 @@ namespace DockerGen.Container
 {
 	public class AddFilesInstruction : Instruction
 	{
-		public override string Description => "Copes files, directories or remote files URLs from `Source` and adds it to the filesystem of the image at the path `Destination`";
+		public override string Description => "Copies files, directories or remote files URLs from `Source` and adds it to the filesystem of the image at the path `Destination`";
 		public override string Prefix => "ADD";
+		public override string DisplayName => "Add files from local or remote source";
 
-		[JsonInclude]
+        [JsonInclude]
 		public string LocalOrRemoteSource { get; set; } = "/src";
 		[JsonInclude]
 		public string Destination { get; set; } = "/app";
