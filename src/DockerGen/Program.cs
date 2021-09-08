e674a3546bcfdc90427c49d1221c9407542d1d87
frodehus <frode.hus@outlook.com>
2021-09-08 08:58:49 +0200

    more transitions to MatBlazor

--- src/DockerGen/Program.cs
+++ src/DockerGen/Program.cs
@@ -1,5 +1,4 @@
 using Blazored.LocalStorage;
-using Blazored.Toast;
 using DockerGen.Components;
 using DockerGen.Components.Instructions;
 using DockerGen.Container;
@@ -23,8 +22,15 @@ namespace DockerGen
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DockerGen.Api"));
             builder.Services.AddScoped<ApiService>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredToast();
             builder.Services.AddMatBlazor();
+            builder.Services.AddMatToaster(config =>
+            {
+                config.Position = MatToastPosition.TopRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = true;
+                config.ShowCloseButton = true;
+                config.VisibleStateDuration = 3000;
+            });
             builder.Services.AddContainerService(o =>
             {
                 o.MapUIComponent<FromInstruction, FromImage>();
