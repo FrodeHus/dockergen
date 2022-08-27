4924a8a0d62ce8cab5fbe2801afcf30678135f15
frodehus <frode.hus@outlook.com>
2022-08-27 21:18:22 +0200

    added missing file

---
+++ src/DockerLib/CodeAnalysis/Syntax/SyntaxTrivia.cs
@@ -0,0 +1,20 @@
+using DockerLib.CodeAnalysis.Text;
+
+namespace DockerLib.CodeAnalysis.Syntax;
+
+public class SyntaxTrivia
+{
+    public SyntaxTrivia(SourceDockerfile source, SyntaxKind kind, int position, string text)
+    {
+        Source = source;
+        Kind = kind;
+        Position = position;
+        Text = text;
+    }
+
+    public SourceDockerfile Source { get; }
+    public SyntaxKind Kind { get; }
+    public int Position { get; }
+    public string Text { get; }
+    public TextSpan Span => new(Position, Text?.Length ?? 0);
+}
