ab08251ece41b8f31a97a50d8e3eb28c7d8e6ad5
frodehus <frode.hus@outlook.com>
2022-08-28 00:07:09 +0200

    initial repl

---
+++ src/DockerLib/DockerREPL/Program.cs
@@ -0,0 +1,26 @@
+﻿using DockerLib.CodeAnalysis.Syntax;
+using DockerLib.CodeAnalysis.Text;
+using PrettyPrompt;
+using PrettyPrompt.Consoles;
+using PrettyPrompt.Highlighting;
+
+var console = new SystemConsole();
+var prompt = new Prompt(configuration: new PromptConfiguration(
+    prompt: new FormattedString(">>> ", new FormatSpan(0, 1, AnsiColor.Red), new FormatSpan(1, 1, AnsiColor.Yellow), new FormatSpan(2, 1, AnsiColor.Green))
+));
+console.WriteLine("Welcome to the Dockerfile REPL (Read Eval Print Loop)!");
+console.WriteLine("Type Dockerfile expressions and statements at the prompt and press Enter to evaluate them.");
+console.WriteLine($"Type 'help to learn more, and type 'exit' to quit.");
+console.WriteLine(string.Empty);
+while (true)
+{
+    var response = await prompt.ReadLineAsync();
+    if (response.IsSuccess)
+    {
+        if (response.Text == "exit") break;
+        var parser = new Parser(SourceDockerfile.From(response.Text));
+        var instructions = parser.Parse();
+        var instruction = instructions.FirstOrDefault();
+        console.WriteLine(instruction?.ToString());
+    }
+}
